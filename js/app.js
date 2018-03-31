0c7c8461e78f72ba5356c9ee88fee8882f2f5238
Lindsay M <lminami23@punahou.edu>
2018-03-31 11:01:37 -1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+	setTimeout(function(){
+		var loadingContainer = document.getElementById('loading-container');
+		var mainContainer = document.getElementById('main-container');
+		loadingContainer.style.display = 'none';	
+		mainContainer.style.display = 'inline';
+	}, 1000);
+	//change to 3000 later
+
+	setTimeout(function(){
+		var modal = document.getElementById('modal');
+		var span = document.getElementById('close');
+		var request = document.getElementById('requestBtn')
+		var modal2 = document.getElementById('other-modal-content')
+		modal.style.display = 'inline';
+		span.addEventListener('click', function(){
+			modal.style.display = 'none';
+		})
+		request.addEventListener('click', function(){
+			modal2.style.display = 'inline'
+			request.style.display = 'none'
+		})
+	}, 1000)
+}
