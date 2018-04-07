447a297ff5843007639e474da6ca9cf7109fd833
Lindsay M <lminami23@punahou.edu>
2018-04-07 11:01:05 -1000

    yeet

--- js/app.js
+++ js/app.js
@@ -10,15 +10,15 @@ window.onload = function(){
 	setTimeout(function(){
 		var modal = document.getElementById('modal');
 		var span = document.getElementById('close');
-		var request = document.getElementById('requestBtn')
-		var modal2 = document.getElementById('other-modal-content')
+		var request = document.getElementById('requestBtn');
+		var modal2 = document.getElementById('gform');
 		modal.style.display = 'inline';
 		span.addEventListener('click', function(){
 			modal.style.display = 'none';
 		})
 		request.addEventListener('click', function(){
 			modal2.style.display = 'inline'
-			//request.style.display = 'none'
+			request.style.display = 'none'
 		})
 	}, 1000)
 }
